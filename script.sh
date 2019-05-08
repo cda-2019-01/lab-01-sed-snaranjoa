a4ae4690b0e290febfbe8436746908dcf0d2e263
snaranjoa <50253439+snaranjoa@users.noreply.github.com>
2019-05-07 23:16:03 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,6 @@
 # Escriba su código aquí
+sed 's/,/./g' data.csv > data1.csv
+sed 's/;/,/g' data1.csv > data2.csv
+tr '/' '-' < data2.csv > data3.csv
+sed 's/-\([0-9][0-9]\),/-20\1,/' data3.csv > data4.csv
+sed 's/\([0-9][0-9]\)-\([0-9][0-9]\)-\([0-9][0-9][0-9][0-9]\),/\3-\2-\1,/' data4.csv > data5.csv
